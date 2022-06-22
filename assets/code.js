de534bbf12ee97a2e209ccf4c70e85b25516abe0
ismail saoudi <ismail.saoudi@icloud.com>
2022-06-22 09:42:50 -0400

    last test before go live

--- assets/code.js
+++ assets/code.js
@@ -1,14 +1,27 @@
 
- `use strict`
+ 
  var datetime = new Date();
  document.getElementById("currentDay").textContent = datetime; //it will print on html page
  
- `use strict`;
- function refreshTime() {
+ 
+ function refreshTime() { // this will format the date and initialize it 
    const timeDisplay = document.getElementById("currentDay");
    const dateString = new Date().toLocaleString();
    const formattedString = dateString.replace(", ", " - ");
    timeDisplay.textContent = formattedString;
  }
    setInterval(refreshTime, 1000);
+
+
+   document.querySelector(".btn").addEventListener("click", function ()
+   {
+       var data = document.getElementById("floatingTextarea").value ;
+       localStorage.setItem("event", data) ;
+       alert("event saved") ;
+       console.log("event saved")
+   } , true);
+
+
+  
+
 
